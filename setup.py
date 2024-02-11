7b420ebe343393a51da20e80c1bb081fac33f1ab
めがねこ <29650686+MeGaNeKoS@users.noreply.github.com>
2024-02-12 02:41:58 +0700

    build: Increment release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='htmlsnippets',
-    version='0.0.1',
+    version='0.0.2',
     description='Renders and processes HTML snippets',
     author='めがねこ',
     author_email='meganeko@example.com',
